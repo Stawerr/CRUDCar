e91feabf5db6fc4caead56f5bcc611676f0770eb
Stawerr <renato-silva_16@hotmail.com>
2022-06-01 23:02:38 +0100

    soft delete with foreign key type success2

--- resources/views/components/table_cars/cars.blade.php
+++ resources/views/components/table_cars/cars.blade.php
@@ -1,3 +1,13 @@
+@if (session('status'))
+    <div class="alert
+        alert-success
+        alert-dismissible fade show" role="alert">
+        {{ session('status') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
 <h1>Cars</h1>
 <table class="table table-striped table-dark">
     <thead>
