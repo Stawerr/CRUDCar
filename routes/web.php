d82ae4e929e13b86457642326994b09581a5c742
Renato Rodrigues <Renato.Rodrigues.T0121547@edu.atec.pt>
2022-06-08 21:00:56 +0100

    admin route

--- routes/web.php
+++ routes/web.php
@@ -22,19 +22,24 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/brands','BrandController@index');
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => 'admin'], function () {
     Route::get('/brands/create','BrandController@create');
     Route::post('/brands/','BrandController@store');
+});
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/brands/{brand}/edit','BrandController@edit');
     Route::put('/brands/{brand}', 'BrandController@update');
     Route::delete('/brands/{brand}', 'BrandController@destroy');
 });
 Route::get('/brands/{brand}','BrandController@show');
 
+
 Route::get('/cars','CarController@index');
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => 'admin'], function () {
     Route::get('/cars/create','CarController@create');
     Route::post('/cars/','CarController@store');
+});
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/cars/{car}/edit','CarController@edit');
     Route::put('/cars/{car}', 'CarController@update');
     Route::delete('/cars/{car}', 'CarController@destroy');
